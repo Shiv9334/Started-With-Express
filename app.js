37ee394c18f422d4130bf1963f58cdfc269a16f5
Shiv <shivkumar9334@gmail.com>
2023-03-18 10:28:30 +0530

    second commit

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const http = require('http');
 const express = require('express');
+const { text } = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('A')
+app.use(bodyParser.urlencoded({extended : false}));
+
+
+app.use('/add-product',(req,res,next) => {
+    console.log('add-product');
+    res.send('<form action="/product" method ="POST"><input type="text" name ="title"><button type = "submit">Add product</button></input></form> ');
+})
+app.use('/product',(req,res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+})
+app.use('/', (req, res, next) => {
+    console.log('This always runs');
+   res.send('Welcome to express js page');
     next();
 })
-app.use((req, res, next)  => {
-    console.log('B')
-    
-})
+
 
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(8000);
